23a7e8da801e0c1682ed3849566df1e7441d044c
xCONFLiCTiONx <mikehall.outbox@gmail.com>
2021-03-03 17:21:17 -0600

    Release 1.7

--- ImageViewer/Properties/AssemblyInfo.cs
+++ ImageViewer/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Windows;
 
     ResourceDictionaryLocation.SourceAssembly
 )]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
